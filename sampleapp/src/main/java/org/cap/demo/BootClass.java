5a093fe666321c2b58075abf638ffeda25929591
kishalayb18 <kisbhatt@LIN51014704.corp.capgemini.com>
2021-09-20 18:18:49 +0530

    hello first dev

--- sampleapp/src/main/java/org/cap/demo/BootClass.java
+++ sampleapp/src/main/java/org/cap/demo/BootClass.java
@@ -3,10 +3,19 @@ package org.cap.demo;
 public class BootClass {
 	public static void main(String[] args) {
 		System.out.println("Hello!");
+		System.out.println("Hello from first dev!");
 	}
 
 	public static void show()
 	{
 		System.out.println("show method here");
+		
+		int[] arr= {10,20,30};
+		for(int i=0; i<3; i++)
+		{
+			System.out.println(arr[i]);
+		}
+		
 	}
+	
 }
